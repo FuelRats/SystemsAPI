0f00e4139df44eb46ef6f5da55d1e15bfceee32b
Kenneth Aalberg <kenneaal@gmail.com>
2020-02-02 18:44:01 +0100

    Set up Alembic initial version without indexes.

--- systems_api/systems_api/models/__init__.py
+++ systems_api/systems_api/models/__init__.py
@@ -6,6 +6,11 @@ import zope.sqlalchemy
 # import or define all models here to ensure they are attached to the
 # Base.metadata prior to any initialization routines
 from .mymodel import MyModel  # flake8: noqa
+from .star import Star
+from .body import Body
+from .populatedsystem import PopulatedSystem
+from .station import Station
+from .system import System
 
 # run configure_mappers after defining all of the models to ensure
 # all relationships can be setup
