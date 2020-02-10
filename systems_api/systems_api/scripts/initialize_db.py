5f7111be47782365682293ebbbc39112f2e4c80c
Kenneth Aalberg <kenneaal@gmail.com>
2020-02-10 15:27:17 +0100

    Clean up initialize_db, drop ODO. Manual injects for the win.

--- systems_api/systems_api/scripts/initialize_db.py
+++ systems_api/systems_api/scripts/initialize_db.py
@@ -1,19 +1,55 @@
 import argparse
+import os
 import sys
+from bz2 import BZ2Decompressor
+from datetime import datetime, timedelta
 
+import requests
 from pyramid.paster import bootstrap, setup_logging
 from sqlalchemy.exc import OperationalError
 
 from .. import models
 
 
-def setup_models(dbsession):
+def setup_models(dbsession, env):
     """
     Add or update models / fixtures in the database.
 
     """
     model = models.mymodel.MyModel(name='one', value=1)
     dbsession.add(model)
+    settings = env['registry']['settings']
+    filelist = ['systemsWithCoordinates', 'systemsWithoutCoordinates', 'systemsPopulated', 'stars', 'bodies']
+    if 'stardb_host' not in settings:
+        print("Your config does not specify a host for downloading E:D galaxy map data. You will have to"
+              "inject the data manually into the database.")
+        return
+    host = settings['stardb_host']
+    neededfiles = []
+    for file in filelist:
+        if os.path.isfile(f'{file}.csv.bz2'):
+            if datetime.fromtimestamp(os.path.getmtime(f'{file}.csv.bz2')) > datetime.today() - timedelta(
+                    days=7):
+                print(f"Using cached {file}.csv.bz2")
+            else:
+                print(f"Need to download {file} (Too old)")
+                neededfiles.append(file)
+        else:
+            print(f"Need to download {file} (Missing)")
+            neededfiles.append(file)
+    if neededfiles:
+        for file in neededfiles:
+            print(f"Downloading {file}.csv from Spansh...")
+            r = requests.get(f"{host}/{file}.csv.bz2", stream=True)
+        with open('{file}.csv.bz2', 'wb') as f:
+            for chunk in r.iter_content(chunk_size=4096):
+                if chunk:
+                    f.write(chunk)
+    print("Decompressing files...")
+    with open('systemsWithCoordinates.csv', 'wb') as outfile, open('systemsWithCoordinates.csv.bz2', 'rb') as infile:
+        decompressor = BZ2Decompressor()
+        for data in iter(lambda : infile.read(100*1024), b''):
+            outfile.write(decompressor.decompress(data))
 
 
 def parse_args(argv):
@@ -33,7 +69,7 @@ def main(argv=sys.argv):
     try:
         with env['request'].tm:
             dbsession = env['request'].dbsession
-            setup_models(dbsession)
+            setup_models(dbsession, env)
     except OperationalError:
         print('''
 Pyramid is having a problem using your SQL database.  The problem
