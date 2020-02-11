9b0260623df69d5e7b6b3967f7a980b28a2a941e
Kenneth Aalberg <kenneaal@gmail.com>
2020-02-11 13:06:49 +0100

    Add System indexes.

--- systems_api/systems_api/models/system.py
+++ systems_api/systems_api/models/system.py
@@ -2,8 +2,10 @@ from sqlalchemy import (
     Column,
     BigInteger,
     Text,
-    DateTime
-)
+    DateTime,
+    Index,
+    Float,
+    func)
 
 from sqlalchemy.dialects.postgresql import JSONB
 
@@ -17,3 +19,15 @@ class System(Base):
     coords = Column(JSONB)
     date = Column(DateTime)
     date.info.update({'pyramid_jsonapi': {'visible': False}})
+
+
+Index('system_idx_id64', System.id64, unique=True)
+Index('system_idx_name_gin', System.name, postgresql_using='gin',
+      postgresql_ops={'description': 'gin_trgm_ops'})
+Index('system_idx_name_btree', System.name, postgresql_using='btree')
+Index('system_idx_name_soundex', System.name, func.soundex(System.name))
+Index('system_idx_name_dmetaphone', System.name, func.dmetaphone(System.name))
+Index('system_idx_coords', System.coords['x'].cast(Float))
+Index('system_idx_coords', System.coords['y'].cast(Float))
+Index('system_idx_coords', System.coords['z'].cast(Float))
+
