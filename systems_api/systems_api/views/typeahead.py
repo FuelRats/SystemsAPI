5bb17ae7f856d2dee534c59da1f6d015e7c1aecf
Kenneth Aalberg <kenneaal@gmail.com>
2020-02-14 23:31:20 +0100

    Fix missing method call in typeahead.py.

--- systems_api/systems_api/views/typeahead.py
+++ systems_api/systems_api/views/typeahead.py
@@ -26,7 +26,7 @@ def search(request):
     if len(name) < 3:
         return exc.HTTPBadRequest(detail="Typeahead term too short (Minimum 3 characters)")
     result = request.dbsession.query(System).filter(System.name.ilike(f"{name}%")).limit(10)
-    if result.count > 0:
+    if result.count() > 0:
         candidates = []
         for row in result:
             candidates.append(row['name'])
