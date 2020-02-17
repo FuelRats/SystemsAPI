bd4cf8b00a9eb46f1e857ece2d7731ac241cd0e2
Kenneth Aalberg <kenneaal@gmail.com>
2020-02-17 20:48:14 +0100

    Free extra oops with every order.

--- systems_api/systems_api/views/search.py
+++ systems_api/systems_api/views/search.py
@@ -78,7 +78,8 @@ def search(request):
         sql = text(f"SELECT *, similarity(name, '{name}') AS similarity FROM systems "
                    f"WHERE dmetaphone(name) = dmetaphone('{name}') ORDER BY similarity DESC LIMIT {str(limit)}")
     if searchtype == "fulltext":
-        sql = text(f"SELECT name, id64 FROM systems WHERE name LIKE '{name}%' ORDER BY similarity DESC LIMIT {str(limit)}")
+        sql = text(f"SELECT name, id64, similarity(name, '{name}') AS similarity FROM systems "
+                   f"WHERE name LIKE '{name}%' ORDER BY similarity DESC LIMIT {str(limit)}")
     if not result:
         # We haven't gotten a ORM result yet, execute manual SQL.
         result = request.dbsession.execute(sql)
