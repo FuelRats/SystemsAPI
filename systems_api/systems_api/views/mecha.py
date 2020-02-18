e34c99dde5d6f25510f92ae58e63e392881cf14f
Kenneth Aalberg <kenneaal@gmail.com>
2020-02-18 01:26:23 +0100

    Expose snowflake permit systems in mecha queries.

--- systems_api/systems_api/views/mecha.py
+++ systems_api/systems_api/views/mecha.py
@@ -32,7 +32,9 @@ def mecha(request):
     for candidate in pmatch:
         candidates.append({'name': candidate.name, 'similarity': 1,
                            'id64': candidate.id64,
-                           'permit_required': True if candidate.id64 in perm_systems else False})
+                           'permit_required': True if candidate.id64 in perm_systems else False,
+                           'permit_name': permsystems.get(candidate.id64).permit_name or None
+                           })
     if len(candidates) > 0:
         return {'meta': {'name': name, 'type': 'Perfect match'}, 'data': candidates}
     # Try an indexed ilike on the name, no wildcard.
@@ -41,7 +43,9 @@ def mecha(request):
     for candidate in result:
         candidates.append({'name': candidate[0].name, 'similarity': candidate[1],
                            'id64': candidate[0].id64,
-                           'permit_required': True if candidate[0].id64 in perm_systems else False})
+                           'permit_required': True if candidate[0].id64 in perm_systems else False,
+                           'permit_name': permsystems.get(candidate.id64).permit_name or None
+                           })
     if len(candidates) < 1:
         # Try an ILIKE with a wildcard at the end.
         print("Strat: ILIKE wildcard")
@@ -50,7 +54,9 @@ def mecha(request):
         for candidate in pmatch:
             candidates.append({'name': candidate[0].name, 'similarity': candidate[1],
                                'id64': candidate[0].id64,
-                               'permit_required': True if candidate[0].id64 in perm_systems else False})
+                               'permit_required': True if candidate[0].id64 in perm_systems else False,
+                               'permit_name': permsystems.get(candidate.id64).permit_name or None
+                               })
         if len(candidates) > 0:
             return {'meta': {'name': name, 'type': 'wildcard'}, 'data': candidates}
         # Try a trigram similarity search if English-ish system name
@@ -63,7 +69,9 @@ def mecha(request):
                     # candidates.append({'name': candidate[0].name, 'similarity': "1.0"}
                     candidates.append({'name': candidate[0].name, 'similarity': candidate[1],
                                        'id64': candidate[0].id64,
-                                       'permit_required': True if candidate[0].id64 in perm_systems else False})
+                                       'permit_required': True if candidate[0].id64 in perm_systems else False,
+                                       'permit_name': permsystems.get(candidate.id64).permit_name or None
+                                       })
 
         else:
             # Last effort, try a dimetaphone search.
@@ -75,7 +83,8 @@ def mecha(request):
             for candidate in result:
                 candidates.append({'name': candidate.name, 'similarity': candidate.similarity,
                                    'id64': candidate.id64,
-                                   'permit_required': True if candidate.id64 in perm_systems else False})
+                                   'permit_required': True if candidate.id64 in perm_systems else False,
+                                   'permit_name': permsystems.get(candidate.id64).permit_name or None})
     if len(candidates) < 1:
         # We ain't got shit. Give up.
         return {'meta': {'name': name, 'error': 'No hits.'}}
