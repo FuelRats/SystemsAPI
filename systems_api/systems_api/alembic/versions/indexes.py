bf68ddf835b680e187d603d29f61c7fb981894cf
Kenneth Aalberg <kenneaal@gmail.com>
2020-02-18 14:43:51 +0100

    Betterify mecha searches. Add dmeta and soundex indexes.

--- systems_api/systems_api/alembic/versions/indexes.py
+++ systems_api/systems_api/alembic/versions/indexes.py
@@ -1,6 +1,4 @@
 from alembic import op
-import sqlalchemy as sa
-from sqlalchemy.dialects import postgresql
 
 revision = 'indexes'
 down_revision = 'unindexed'
@@ -23,7 +21,12 @@ def upgrade():
     op.create_index('psystem_idx_systemid64', 'populated_systems', ['name'], unique=False)
     op.create_index('system_idx_id64', 'systems', ['id64'], unique=True)
     op.create_index('system_idx_name_btree', 'systems', ['name'], unique=False, postgresql_using='btree')
-    op.create_index('system_idx_name_gin', 'systems', ['name'], unique=False, postgresql_using='gin', postgresql_ops={'name': 'gin_trgm_ops'})
+    op.create_index('system_idx_name_gin', 'systems', ['name'], unique=False, postgresql_using='gin',
+                    postgresql_ops={'name': 'gin_trgm_ops'})
+    op.create_index('system_idx_name_soundex', 'systems', ['name'], unique=False, postgresql_using='btree',
+                    postgres_ops={'name': 'soundex'})
+    op.create_index('system_idx_name_dmetaphone', 'systems', ['name'], unique=False, postgresql_using='btree',
+                    postgres_ops={'name': 'dmetaphone'})
     op.create_index('body_idx_id64', 'bodies', ['id64'], unique=True)
     op.create_index('body_idx_systemid64', 'bodies', ['systemId64'], unique=False)
     op.create_index('star_idx_id64', 'stars', ['id64'], unique=True)
@@ -37,6 +40,8 @@ def downgrade():
     op.drop_index('body_idx_id64', table_name='bodies')
     op.drop_index('system_idx_name_gin', table_name='systems')
     op.drop_index('system_idx_name_btree', table_name='systems')
+    op.drop_index('system_idx_name_soundex', table_name='systems')
+    op.drop_index('system_idx_name_dmetaphone', table_name='systems')
     op.drop_index('system_idx_id64', table_name='systems')
     op.drop_index('psystem_idx_systemid64', table_name='populated_systems')
     op.drop_index('psystem_idx_id64', table_name='populated_systems')
--- systems_api/systems_api/views/mecha.py
+++ systems_api/systems_api/views/mecha.py
@@ -16,7 +16,7 @@ def mecha(request):
     :return: A JSON response
     """
     if 'name' not in request.params:
-        return {'meta': {'error': 'No search term in \'name\' parameter!'}}
+        return exc.HTTPBadRequest(detail="Missing 'name' parameter.")
     candidates = []
     permsystems = request.dbsession.query(Permits)
     perm_systems = []
@@ -25,11 +25,9 @@ def mecha(request):
     name = request.params['name']
     if len(name) < 3:
         return exc.HTTPBadRequest(detail="Search term too short (Minimum 3 characters)")
-    # Temp!
-    #query = request.dbsession.query(System).filter(func.dmetaphone(System.name) == func.dmetaphone(name))
-    #print(query)
-    pmatch = request.dbsession.query(System).filter(System.name == name)
-    for candidate in pmatch:
+    # Check for immediate match, case insensitive.
+    query = request.dbsession.query(System).filter(System.name.ilike(name))
+    for candidate in query:
         candidates.append({'name': candidate.name, 'similarity': 1,
                            'id64': candidate.id64,
                            'permit_required': True if candidate.id64 in perm_systems else False,
@@ -37,54 +35,41 @@ def mecha(request):
                            })
     if len(candidates) > 0:
         return {'meta': {'name': name, 'type': 'Perfect match'}, 'data': candidates}
-    # Try an indexed ilike on the name, no wildcard.
-    result = request.dbsession.query(System, func.similarity(System.name, name).label('similarity')).\
-        filter(System.name.ilike(name)).limit(10).from_self().order_by(func.similarity(System.name, name).desc())
-    for candidate in result:
+    # Try soundex and dmetaphone matches on the name, look for low levenshtein distances.
+    qtext = text("select *, levenshtein(name, :name) as lev from systems where dmetaphone(name) "
+                 "= dmetaphone(:name) OR soundex(name) = soundex(:name) order by lev")
+    query = request.dbsession.query(System).from_statement(qtext).params(name=name)
+    for candidate in query:
+        candidates.append({'name': candidate[0].name, 'distance': candidate[1],
+                           'id64': candidate[0].id64,
+                           'permit_required': True if candidate[0].id64 in perm_systems else False,
+                           'permit_name': permsystems.get(candidate[0].id64).permit_name or None
+                           })
+    if len(candidates) > 0:
+        return {'meta': {'name': name, 'type': 'dmeta+soundex'}, 'data': candidates}
+    # Try an ILIKE with wildcard on end. Slower.
+    query = request.dbsession.query(System, func.similarity(System.name, name).label('similarity')).\
+        filter(System.name.ilike(name+"%")).limit(10).from_self().order_by(func.similarity(System.name, name).desc())
+    for candidate in query:
         candidates.append({'name': candidate[0].name, 'similarity': candidate[1],
                            'id64': candidate[0].id64,
                            'permit_required': True if candidate[0].id64 in perm_systems else False,
-                           'permit_name': permsystems.get(candidate.id64).permit_name or None
+                           'permit_name': permsystems.get(candidate[0].id64).permit_name or None if candidate[0].id64 in perm_systems else False
                            })
-    if len(candidates) < 1:
-        # Try an ILIKE with a wildcard at the end.
-        print("Strat: ILIKE wildcard")
-        pmatch = request.dbsession.query(System, func.similarity(System.name, name).label('similarity')).\
-            filter(System.name.ilike(name+"%")).limit(10).from_self().order_by(func.similarity(System.name, name).desc())
+    if len(candidates) > 0:
+        return {'meta': {'name': name, 'type': 'wildcard'}, 'data': candidates}
+    # Try a GIN trigram similarity search on the entire database. Slow as hell.
+    pmatch = request.dbsession.query(System, func.similarity(System.name, name).label('similarity')). \
+        filter(System.name % name).limit(10).from_self().order_by(func.similarity(System.name, name).desc())
+    if pmatch.count() > 0:
         for candidate in pmatch:
+            # candidates.append({'name': candidate[0].name, 'similarity': "1.0"}
             candidates.append({'name': candidate[0].name, 'similarity': candidate[1],
                                'id64': candidate[0].id64,
                                'permit_required': True if candidate[0].id64 in perm_systems else False,
-                               'permit_name': permsystems.get(candidate[0].id64).permit_name or None if candidate[0].id64 in perm_systems else False
+                               'permit_name': permsystems.get(candidate[0].id64).permit_name or None if candidate[
+                                                                                                            0].id64 in perm_systems else False
                                })
-        if len(candidates) > 0:
-            return {'meta': {'name': name, 'type': 'wildcard'}, 'data': candidates}
-        # Try a trigram similarity search if English-ish system name
-        if len(name.split(' ')) < 2:
-            print("Strat: Trigram")
-            pmatch = request.dbsession.query(System, func.similarity(System.name, name).label('similarity')).\
-                filter(System.name % name).limit(10).from_self().order_by(func.similarity(System.name, name).desc())
-            if pmatch.count() > 0:
-                for candidate in pmatch:
-                    # candidates.append({'name': candidate[0].name, 'similarity': "1.0"}
-                    candidates.append({'name': candidate[0].name, 'similarity': candidate[1],
-                                       'id64': candidate[0].id64,
-                                       'permit_required': True if candidate[0].id64 in perm_systems else False,
-                                       'permit_name': permsystems.get(candidate[0].id64).permit_name or None if candidate[0].id64 in perm_systems else False
-                                       })
-
-        else:
-            # Last effort, try a dimetaphone search.
-            print("Strat: Dmeta")
-            sql = text(f"SELECT *, similarity(name, '{name}') AS similarity FROM systems "
-                       f"WHERE dmetaphone(name) = dmetaphone('{name}') ORDER BY similarity DESC LIMIT 10")
-
-            result = request.dbsession.execute(sql)
-            for candidate in result:
-                candidates.append({'name': candidate.name, 'similarity': candidate.similarity,
-                                   'id64': candidate.id64,
-                                   'permit_required': True if candidate.id64 in perm_systems else False,
-                                   'permit_name': permsystems.get(candidate.id64).permit_name or None})
     if len(candidates) < 1:
         # We ain't got shit. Give up.
         return {'meta': {'name': name, 'error': 'No hits.'}}
