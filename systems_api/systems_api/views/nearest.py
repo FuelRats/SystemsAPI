20bfbcf35503c767bc84274224d5f5b92dce4a4c
Kenneth Aalberg <kenneaal@gmail.com>
2020-05-01 23:35:47 +0200

    Debug values are spoopy!

--- systems_api/systems_api/views/nearest.py
+++ systems_api/systems_api/views/nearest.py
@@ -103,7 +103,7 @@ def nearest_scoopable(request):
     candidates = request.dbsession.query(System). \
         filter(and_(System.coords['x'].as_float().between((x - cube), (x + cube)),
                     System.coords['y'].as_float().between((y - cube), (y + cube)),
-                    System.coords['z'].as_float().between((z - cube), (z + cube)))).join(Star).limit(50000).all()
+                    System.coords['z'].as_float().between((z - cube), (z + cube)))).join(Star).limit(500).all()
     results = []
     for candidate in candidates:
         for star in candidate.stars:
