bdda3b0044cefe269bd81f7c78b72971851ecc26
Kenneth Aalberg <kenneaal@gmail.com>
2020-05-08 17:07:48 +0200

    Cache heatmap in json storage, regenerate daily on first request. Cleanup.

--- systems_api/systems_api/views/heatmap.py
+++ systems_api/systems_api/views/heatmap.py
@@ -1,24 +1,24 @@
+import json
+from datetime import datetime, timedelta
+
 from pyramid.view import (
     view_config,
     view_defaults
 )
-from sqlalchemy import text, func
-from ..models import System, Permits
+from ..models import System
 import pyramid.httpexceptions as exc
 import psycopg2
+import os
 
 
-# Temporary static endpoint for API v2. V3 gets a more thorough implementation.
-@view_defaults(renderer='../templates/mytemplate.jinja2')
-@view_config(route_name='heatmap', renderer='json')
-def heatmap(request):
+def generate_heatmap(request):
     global conn
     try:
         conn = psycopg2.connect(host="192.168.100.10", database="fuelratsapi", user="fuelrats")
         cur = conn.cursor()
         cur.execute('SELECT system as "System", COUNT(system) as "Rescues" FROM "Rescues" WHERE '
                     '"deletedAt" IS NULL and position(\': false\' in "data"::json#>>\'{markedForDeletion}\')>0 '
-                    'GROUP BY system ORDER BY count("system") DESC LIMIT 1000')
+                    'GROUP BY system ORDER BY count("system") DESC')
         rows = cur.fetchall()
         heatmap = []
         for row in rows:
@@ -30,7 +30,26 @@ def heatmap(request):
             heatmap.append({row[0]: {'rescues': row[1], 'coords': coords}})
         cur.close()
         conn.close()
+        hj = open('heatmap.json', mode='w')
+        json.dump(heatmap, hj)
         return heatmap
     except (Exception, psycopg2.Error) as error:
         print(f"Oops! {error}")
 
+
+# Temporary static endpoint for API v2. V3 gets a more thorough implementation.
+@view_defaults(renderer='../templates/mytemplate.jinja2')
+@view_config(route_name='heatmap', renderer='json')
+def heatmap(request):
+    if os.path.isfile(f'heatmap.json'):
+        if datetime.fromtimestamp(os.path.getmtime(f'heatmap.json')) > datetime.today() - timedelta(
+                days=1):
+            print("Generating new heatmap...")
+            generate_heatmap(request)
+        else:
+                hj = open('heatmap.json', mode='r')
+                heatmap = json.load(hj)
+                return heatmap
+    else:
+        print("First run, generating heatmap...")
+        generate_heatmap(request)
