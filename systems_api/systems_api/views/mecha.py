0c4129c7cc3cf1cc86caf73fa462d2750152fff0
Kenneth Aalberg <kenneaal@gmail.com>
2020-09-09 22:59:51 +0200

    Add pgname detection to shortcut directly to trigram search. Switch to qtext for trigram to stop SQLA from double-toking. Puff and pass, darn you!

--- systems_api/systems_api/views/mecha.py
+++ systems_api/systems_api/views/mecha.py
@@ -6,6 +6,7 @@ from sqlalchemy import text, func
 from ..models import System, Permits
 import pyramid.httpexceptions as exc
 from ..utils.util import checkpermitname, resultstocandidates
+from ..utils.pgnames import is_pg_system_name
 
 
 @view_defaults(renderer='../templates/mytemplate.jinja2')
@@ -37,6 +38,22 @@ def mecha(request):
                            })
     if len(candidates) > 0:
         return {'meta': {'name': name, 'type': 'Perfect match'}, 'data': candidates}
+    if is_pg_system_name(name):
+        # If the system is a PGName, don't try soundex and dmeta first, as they are most likely to fail.
+        qtext = text("select *, levenshtein(lower(name), lower(:name)) as lev from systems where name % :name"
+                     " ORDER BY lev LIMIT 10")
+        pmatch = request.dbsession.query(System, "lev").from_statement(qtext).params(name=name).all()
+        for candidate in pmatch:
+            # candidates.append({'name': candidate[0].name, 'similarity': "1.0"}
+            candidates.append({'name': candidate[0].name, 'similarity': candidate[1],
+                               'id64': candidate[0].id64,
+                               'permit_required': True if candidate[0].id64 in perm_systems else False,
+                               'permit_name': checkpermitname(candidate[0].id64, permsystems, perm_systems)
+                               })
+            if len(candidates) > 10:
+                break
+        if len(candidates) > 1:
+            return {'meta': {'name': name, 'type': 'gin_trgm'}, 'data': candidates}
     # Try soundex and dmetaphone matches on the name, look for low levenshtein distances.
     qtext = text("select *, levenshtein(lower(name), lower(:name)) as lev from systems where dmetaphone(name) "
                  "= dmetaphone(:name) OR soundex(name) = soundex(:name) order by lev limit 10")
@@ -66,8 +83,9 @@ def mecha(request):
     if len(candidates) > 0:
         return {'meta': {'name': name, 'type': 'wildcard'}, 'data': candidates}
     # Try a GIN trigram similarity search on the entire database. Slow as hell.
-    pmatch = request.dbsession.query(System, func.similarity(System.name, name).label('similarity')). \
-        filter(System.name % name).limit(5000).from_self().order_by(func.similarity(System.name, name).desc())
+    qtext = text("select *, levenshtein(lower(name), lower(:name)) as lev from systems where name % :name"
+                 " ORDER BY lev LIMIT 10")
+    pmatch = request.dbsession.query(System, "lev").from_statement(qtext).params(name=name).all()
     if pmatch.count() > 0:
         for candidate in pmatch:
             # candidates.append({'name': candidate[0].name, 'similarity': "1.0"}
