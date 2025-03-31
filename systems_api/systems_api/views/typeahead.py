4d7b17e687313cb6253f10d314abc263aae22f58
Kenneth Aalberg <kenneaal@gmail.com>
2025-03-31 20:19:45 +0200

    Switch Typeahead search to GIN lowercase indexing. (Much, MUCH more efficient)

--- systems_api/systems_api/views/typeahead.py
+++ systems_api/systems_api/views/typeahead.py
@@ -5,6 +5,7 @@ from pyramid.view import (
     view_defaults
 )
 from pyramid.response import Response
+from sqlalchemy import text
 
 from ..models import System
 import pyramid.httpexceptions as exc
@@ -26,14 +27,24 @@ def search(request):
         'Access-Control-Max-Age': '1728000',
     })
     if 'term' in request.params:
-        name = request.params['term'].upper()
+        name = request.params['term'].lower()
     else:
         return exc.HTTPBadRequest(detail="No name in search request.")
     if len(name) < 3:
         return exc.HTTPBadRequest(detail="Typeahead term too short (Minimum 3 characters)")
-    result = request.dbsession.query(System).filter(System.name.ilike(f"{name}%")).limit(10)
-    if result.count() > 0:
-        candidates = []
-        for row in result:
-            candidates.append(row.name)
-        return candidates
+
+    query = text("""
+                 SET LOCAL work_mem = '100MB';
+                 SELECT name
+                 FROM systems
+                 WHERE lower(name) LIKE lower(:prefix)
+                 ORDER BY name <-> :term DESC
+                     LIMIT 10
+                 """)
+
+    result = request.dbsession.execute(query, {
+        "prefix": f"{name}%",
+        "term": name})
+
+    candidates = [row[0] for row in result]
+    return candidates
